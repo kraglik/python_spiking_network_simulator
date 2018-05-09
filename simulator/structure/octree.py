97c3105464ddb5a916b423d617042082e6aaa082
kraglik <igor.kraglik@yandex.ru>
2018-05-09 14:20:35 +0500

    Simplified Octree class (There is no need for local time in each node, because system has global logic time and proceeds one event per moment)

--- simulator/structure/octree.py
+++ simulator/structure/octree.py
@@ -1,18 +1,16 @@
 from simulator.core import Actor, EventBus
+from simulator.core.proxy import Proxy
 from simulator.math.types import Point
 
 
 class OctreeNode(Actor):
 
-    def __init__(self, center: Point, side_len: float, time=0.0, *args, **kwargs):
-        super(OctreeNode, self).__init__(time, *args, **kwargs)
+    def __init__(self, center: Point, side_len: float):
+
         self.center = center
         self.side_len = side_len
-        self.linked_nodes = []
-        self.linked_nodes_notifications = 0
 
     def receive(self, event):
-        self.run()
         return None
 
     def split(self):
